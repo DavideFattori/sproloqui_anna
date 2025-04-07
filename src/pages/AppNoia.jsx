56963b2e1bece2a79b74f4525dfc413de63248ed
DavideFattori <fattorid5@gmail.com>
2025-04-07 20:21:42 +0200

    modifiche

--- src/pages/AppNoia.jsx
+++ src/pages/AppNoia.jsx
@@ -1,36 +1,100 @@
-import { useState, useRef } from "react";
-
+import { useState } from 'react';
+import { Link } from 'react-router';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
+import '../style/storie.css'
 
 export default function AppNoia() {
 
-    let parole = {
-            'numero': "",
-            'nazione/citta': "",
-            'aggettivo': "",
-            'citta': "",
-            'numero2': "",
-            'valuta': "",
-            'verbo': "",
-            'aggettivo2': "",
-            'aggettivo3': "",
-            'verbo2': "",
-            'numero3': "",
-            'verbo3': "",
-            'sostantivoFemminile': "",
-            'verbo4': "",
-            'avverbio': "",
-            'numero4': "",
-        }
+    const [parole, setParole] = useState({
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+    });
+    const [mostraStoriaFinita, setMostraStoriaFinita] = useState(false)
+    const [avvisoParole, setAvvisoParole] = useState(false)
     
+    function ControllaParole() {
+        let tuttiPieni = true
+        Object.values(parole).forEach(value => {
+            if (value === "") {
+                tuttiPieni = false
+            }
+        });
+        if (tuttiPieni) {
+            setAvvisoParole(false)
+            setMostraStoriaFinita(true)
+        }
+        else {
+            setAvvisoParole(true)
+        }
+    }
+
 
 
 
     return (
-        <div className="container-fluid">
-            Mi ritrovo rinchiusa qui, in questa dannatissima (sostantivo). Il tempo sembra non passare, mai le uniche cose che posso fare sono (verbo) (aggettivo) e leggere. Mi manca uscire con i miei amici. Con (nome) ricordo andavamo sempre al (luogo) per (verbo) quei dannatissimi (sostantivo). Non voglio (verbo). (Esclamazione)! Sto (verbo) dite che sono pazza? E' da (numero) (misura di tempo) che sono rinchiusa. Ripenso con nostalgia alle grandi uscite con gli amici, eravamo sempre tantissimi, alle volte anche più di (numero). Mi sentivo come se avessi dovuto (verbo) un (animale). Sicuramente ora non lo farei, come potrei con tutti questi (sostantivo) (aggettivo) è diventato impossibile perfino mangiarsi un (sostantivo). Con questo della bava esce dal mio (parte del corpo). Quasi quasi vado a cucinarmi dello (sostantivo/cibo) (aggettivo), è davvero troppo che lo (verbo). Con questo vado a (verbo). (saluto)
-        </div>
+        <div>non ancora implementato</div>
+        // <div className="container-fluid">
+        //     Mi ritrovo rinchiusa qui, in questa dannatissima 
+        //     (sostantivo)
+        //     . Il tempo sembra non passare, mai le uniche cose che posso fare sono 
+        //     (verbo) 
+        //     (aggettivo) 
+        //     e leggere. Mi manca uscire con i miei amici. Con 
+        //     (nome) 
+        //     ricordo andavamo sempre al 
+        //     (luogo) 
+        //     per 
+        //     (verbo) 
+        //     quei dannatissimi 
+        //     (sostantivo)
+        //     . Non voglio 
+        //     (verbo)
+        //     . 
+        //     (Esclamazione)
+        //     ! Sto 
+        //     (verbo) 
+        //     dite che sono pazza? E' da 
+        //     (numero) 
+        //     (misura di tempo) 
+        //     che sono rinchiusa. Ripenso con nostalgia alle grandi uscite con gli amici, eravamo sempre tantissimi, alle volte anche più di 
+        //     (numero)
+        //     . Mi sentivo come se avessi dovuto 
+        //     (verbo) 
+        //     un 
+        //     (animale)
+        //     . Sicuramente ora non lo farei, come potrei con tutti questi 
+        //     (sostantivo) 
+        //     (aggettivo) 
+        //     è diventato impossibile perfino mangiarsi un 
+        //     (sostantivo)
+        //     . Con questo della bava esce dal mio 
+        //     (parte del corpo)
+        //     . Quasi quasi vado a cucinarmi dello 
+        //     (sostantivo/cibo) 
+        //     (aggettivo)
+        //     , è davvero troppo che lo 
+        //     (verbo)
+        //     . Con questo vado a 
+        //     (verbo)
+        //     . 
+        //     (saluto)
+        // </div>
     )
 }
--- src/pages/AppTg.jsx
+++ src/pages/AppTg.jsx
@@ -1,36 +1,102 @@
-import { useState, useRef } from "react";
-
+import { useState } from 'react';
+import { Link } from 'react-router';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
+import '../style/storie.css'
 
 export default function AppTg() {
 
-    let parole = {
-            'numero': "",
-            'nazione/citta': "",
-            'aggettivo': "",
-            'citta': "",
-            'numero2': "",
-            'valuta': "",
-            'verbo': "",
-            'aggettivo2': "",
-            'aggettivo3': "",
-            'verbo2': "",
-            'numero3': "",
-            'verbo3': "",
-            'sostantivoFemminile': "",
-            'verbo4': "",
-            'avverbio': "",
-            'numero4': "",
-        }
+    const [parole, setParole] = useState({
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+        '': "",
+    });
+    const [mostraStoriaFinita, setMostraStoriaFinita] = useState(false)
+    const [avvisoParole, setAvvisoParole] = useState(false)
     
+    function ControllaParole() {
+        let tuttiPieni = true
+        Object.values(parole).forEach(value => {
+            if (value === "") {
+                tuttiPieni = false
+            }
+        });
+        if (tuttiPieni) {
+            setAvvisoParole(false)
+            setMostraStoriaFinita(true)
+        }
+        else {
+            setAvvisoParole(true)
+        }
+    }
+
 
 
 
     return (
-        <div className="container-fluid">
-            Il (sostantivo) è un continente che ha come confine la (Nazione/luogo) e (sostantivo). Ha molte pianure e (sostantivo) ma scarseggia di laghi, questo a causa del (sostantivo) (aggettivo). Il clima è (aggettivo), cosi tanto che le popolazioni devono (verbo) e (verbo) per sopravvivere. Qui con noi c'è (Nome), è venuto in Italia (numero) (misura di tempo) fa. Ci ha descritto i (sostantivo) che doveva fare per 1 sorso di (liquido/sostanza). Bisogna non (verbo) chi ci sta intorno, neanche se ci (verbo). Questo paese è molto (aggettivo) e (aggettivo), però non è consigliabile per le vostre vacanze. Direi che è abbastanza ovvio data la grande mancanza di (sostantivo) e (sostantivo), tipici prima della (numero) guerra mondiale. E qui con tg attualità abbiamo finito, lascio la parola al mio collega che vi parlerà dei (animale) (aggettivo) e delle loro (sostantivo).
-        </div>
+        <div>non ancora implementato</div>
+        // <div className="container-fluid">
+        //     Il 
+        //     (sostantivo) 
+        //     è un continente che ha come confine la 
+        //     (Nazione/luogo) 
+        //     e 
+        //     (sostantivo)
+        //     . Ha molte pianure e 
+        //     (sostantivo) 
+        //     ma scarseggia di laghi, questo a causa del 
+        //     (sostantivo) 
+        //     (aggettivo)
+        //     . Il clima è 
+        //     (aggettivo)
+        //     , cosi tanto che le popolazioni devono 
+        //     (verbo) 
+        //     e 
+        //     (verbo) 
+        //     per sopravvivere. Qui con noi c'è 
+        //     (Nome)
+        //     , è venuto in Italia 
+        //     (numero) 
+        //     (misura di tempo) 
+        //     fa. Ci ha descritto i 
+        //     (sostantivo) 
+        //     che doveva fare per 1 sorso di 
+        //     (liquido/sostanza)
+        //     . Bisogna non 
+        //     (verbo) 
+        //     chi ci sta intorno, neanche se ci 
+        //     (verbo)
+        //     . Questo paese è molto 
+        //     (aggettivo) 
+        //     e 
+        //     (aggettivo)
+        //     , però non è consigliabile per le vostre vacanze. Direi che è abbastanza ovvio data la grande mancanza di 
+        //     (sostantivo) 
+        //     e 
+        //     (sostantivo)
+        //     , tipici prima della 
+        //     (numero) 
+        //     guerra mondiale. E qui con tg attualità abbiamo finito, lascio la parola al mio collega che vi parlerà dei 
+        //     (animale) 
+        //     (aggettivo) 
+        //     e delle loro 
+        //     (sostantivo)
+        //     .
+        // </div>
     )
 }
